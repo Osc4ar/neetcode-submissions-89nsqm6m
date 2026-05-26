b6065675f4729c0bfd548dae692476726419a11b
Oscar Castillo <oscar.castillo.magana@gmail.com>
2026-05-26 07:10:55 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    '''
+    Using frequency maps of how many times each letter appears in a given string
+    we can compare if both frequency maps are the same.
+
+    Building the frequency map has a Time Complexity of O(n) and a Memory Complexity of O(n).
+    It would be faster than ordering the arrays and comparing them, because ordering would take
+    O(n log(n)) in most cases.
+    '''
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_freq = self.getFrequencies(s)
+        t_freq = self.getFrequencies(t)
+
+        return s_freq == t_freq
+
+    def getFrequencies(self, s: str) -> dict:
+        frequencies = defaultdict(int)
+
+        for c in s:
+            frequencies[c] += 1
+
+        return frequencies
