192b342d5a4f7b0f0ca0b5f5dff1fc75a2ce535c
Oscar Castillo <oscar.castillo.magana@gmail.com>
2026-06-14 22:15:08 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    '''
+    We may use two pointers to have a window of days
+
+    If the left pointer is bigger than right, there is no profit. Move left one position
+    If the left pointer is less or equal than right, there is profit. Move right one position
+
+    Calculate the profit, start with zero and update it with profit = max(profit, current)
+
+    * Pointers have to start in 0 and 1 positions
+    '''
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        left = 0
+        right = 1
+
+        while right < len(prices):
+            print(f'left: {left}, right: {right}')
+            profit = prices[right] - prices[left]
+            result = max(result, profit)
+
+            if profit < 0:
+                # Move the left pointer to the smaller value
+                left = right
+            right += 1
+
+        return result
